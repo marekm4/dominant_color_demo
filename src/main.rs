73c09273c7e475093b00b1dcf5fc03b67859b226
Marek Michalik <marekmichalik43@gmail.com>
2022-02-20 23:08:17 +0100

    Load port from env

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use warp::Filter;
 
 #[tokio::main]
@@ -6,7 +7,8 @@ async fn main() {
     let hello = warp::path!("hello" / String)
         .map(|name| format!("Hello, {}!", name));
 
+    let port = env::var("PORT").unwrap_or("8080".to_string()).parse::<u16>().unwrap();
     warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
+        .run(([127, 0, 0, 1], port))
         .await;
 }
